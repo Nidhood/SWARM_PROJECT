337f19988a8be0d1a110cdd07dcc666f4cd9aa13
Nidhood <ivan.oroz34@gmail.com>
2025-03-13 11:34:38 -0500

    🚀 feat(drone): Add .clang-format, update changelog, and enhance package dependencies

--- drone_gazebo_plugins/include/drone_gazebo_plugins/channel_plugin.hh
+++ drone_gazebo_plugins/include/drone_gazebo_plugins/channel_plugin.hh
@@ -1,50 +1,104 @@
 #pragma once
 
+#include <gz/msgs/float.pb.h> // Library for Gazebo messages.
+#include <gz/sim/Link.hh> // Library for Gazebo simulation link, which is used to get the link of the model.
+#include <gz/sim/Model.hh> // Library for Gazebo simulation model, which is used to get the model.
+#include <gz/sim/System.hh> // Library for Gazebo simulation system, which is used to create plugins
+#include <gz/sim/World.hh> // Library for Gazebo simulation world, which is used to get the world.
+#include <gz/sim/config.hh>     // Library for Gazebo simulation configuration
+#include <gz/transport/Node.hh> // Library for Gazebo transport node, WHICH IS THE MOST IMPORTANT LIBRARY FOR GAZEBO TO COMMUNICATE WITH ROS2
+#include <gz/transport/Publisher.hh> // Library for Gazebo transport publisher, WHICH IS THE MOST IMPORTANT LIBRARY FOR GAZEBO TO PUBLISH WITH ROS2
+#include <memory>                    // Library for shared_ptr
+#include <rclcpp/rclcpp.hpp>         // Library for ROS2 cpp
+#include <std_msgs/msg/float64.hpp>  // Library for ROS2 messages.
 #include <string>
-#include <memory>                       // Library for shared_ptr
-#include <rclcpp/rclcpp.hpp>            // Library for ROS2 cpp 
-#include <gz/sim/System.hh>             // Library for Gazebo simulation system, which is used to create plugins
-#include <gz/sim/config.hh>             // Library for Gazebo simulation configuration
-#include <gz/transport/Node.hh>         // Library for Gazebo transport node, WHICH IS THE MOST IMPORTANT LIBRARY FOR GAZEBO TO COMMUNICATE WITH ROS2
-#include <gz/transport/Publisher.hh>    // Library for Gazebo transport publisher, WHICH IS THE MOST IMPORTANT LIBRARY FOR GAZEBO TO PUBLISH WITH ROS2
-#include <gz/msgs/float.pb.h>           // Library for Gazebo messages.
-#include <std_msgs/msg/float64.hpp>     // Library for ROS2 messages.
-#include <gz/sim/Link.hh>               // Library for Gazebo simulation link, which is used to get the link of the model.
-#include <gz/sim/Model.hh>              // Library for Gazebo simulation model, which is used to get the model.
-#include <gz/sim/World.hh>              // Library for Gazebo simulation world, which is used to get the world.
 
-#include <gz/common/Console.hh>                     // Library for Gazebo console, which is used to print messages when gazebo is running.
-#include "gz/sim/components/AngularVelocity.hh"     // Library for Gazebo simulation angular velocity, which is used to get the angular velocity of the model.
-#include "gz/sim/components/LinearVelocity.hh"      // Library for Gazebo simulation linear velocity, which is used to get the linear velocity of the model.
-#include "gz/sim/components/Pose.hh"                // Library for Gazebo simulation pose, which is used to get the pose of the model.
-#include "gz/sim/components/World.hh"               // Library for Gazebo simulation world, which is used to get the world.
+#include "gz/sim/components/AngularVelocity.hh" // Library for Gazebo simulation angular velocity, which is used to get the angular velocity of the model.
+#include "gz/sim/components/LinearVelocity.hh" // Library for Gazebo simulation linear velocity, which is used to get the linear velocity of the model.
+#include "gz/sim/components/Pose.hh" // Library for Gazebo simulation pose, which is used to get the pose of the model.
+#include "gz/sim/components/World.hh" // Library for Gazebo simulation world, which is used to get the world.
+#include <gz/common/Console.hh> // Library for Gazebo console, which is used to print messages when gazebo is running.
 
-#include <gz/plugin/Register.hh>                    // Library for Gazebo plugin register, which is used to register the plugin.
-#include <gz/math/Vector3.hh>                       // Library for Gazebo math vector3, which is used to get the vector3 of the model.
-#include <gz/math/Pose3.hh>                         // Library for Gazebo math pose3, which is used to get the pose3 of the model.
-#include <gz/sim/Util.hh>                           // Library for Gazebo simulation util, which is used to get the util of the model.
+#include <gz/math/Pose3.hh> // Library for Gazebo math pose3, which is used to get the pose3 of the model.
+#include <gz/math/Vector3.hh> // Library for Gazebo math vector3, which is used to get the vector3 of the model.
+#include <gz/plugin/Register.hh> // Library for Gazebo plugin register, which is used to register the plugin.
+#include <gz/sim/Util.hh> // Library for Gazebo simulation util, which is used to get the util of the model.
 
+#include <gz/sim/Joint.hh> // Library for Gazebo simulation joint, which is used to get the joint of the model.
 #include <trajectory_msgs/msg/joint_trajectory_point.hpp> // Library for ROS2 messages, which is used to get the joint trajectory point of the model.
-#include <gz/sim/Joint.hh>
 
-class Channel : public gz::sim::System, public gz::sim::ISystemPostUpdate, public gz::sim::ISystemConfigure
-{
+#include <gz/msgs/joint_trajectory.pb.h>       // Gazebo JointTrajectory message
+#include <gz/msgs/joint_trajectory_point.pb.h> // Gazebo JointTrajectoryPoint message
+#include <gz/transport/AdvertiseOptions.hh> // For AdvertiseMessageOptions to disable lazy
+
+class Channel : public gz::sim::System,
+                public gz::sim::ISystemPostUpdate,
+                public gz::sim::ISystemConfigure {
     // Life cycle of the plugin, can be:
     // - PreUpdate: Called before the simulation is updated.
     // - PostUpdate: Called after the simulation is updated.
     // - Configure: Called when the plugin is configured.
-public:
-    void PostUpdate(const gz::sim::UpdateInfo& _info,const gz::sim::EntityComponentManager& _ecm) override;
-    void Configure(const gz::sim::Entity &_entity,
-                   const std::shared_ptr<const sdf::Element> &_sdf,
-                   gz::sim::EntityComponentManager &_ecm,
-                   gz::sim::EventManager & /*_eventMgr*/) override;
+  public:
+    // Called on every simulation iteration.
+    void PostUpdate (const gz::sim::UpdateInfo &_info,
+                     const gz::sim::EntityComponentManager &_ecm) override;
 
-private:
+    void Configure (const gz::sim::Entity &_entity,
+                    const std::shared_ptr<const sdf::Element> &_sdf,
+                    gz::sim::EntityComponentManager &_ecm,
+                    gz::sim::EventManager & /*_eventMgr*/) override;
+
+    // Callback for propeller command subscription.
+    void PropellerCommandCallback (
+        const trajectory_msgs::msg::JointTrajectoryPoint::SharedPtr msg);
+
+    // Callback for camera command subscription.
+    void CameraCommandCallback (
+        const trajectory_msgs::msg::JointTrajectoryPoint::SharedPtr msg);
+
+  private:
     gz::transport::Node node_;
-    gz::transport::Node::Publisher pub_;
+    gz::transport::Node::Publisher pub_prop_;
+    gz::transport::Node::Publisher pub_cam_;
+
+    // Default topic names for state publishing.
     std::string propeller_trajectory_node{"propellers_trajectory"};
+    std::string camera_360_trajectory_node{"camera_360_trajectory"};
+
+    // Command topics to be read from SDF (or defaults)
+    std::string topic_propeller_velocity_msgs{"propellers_velocity_topic"};
+    std::string topic_camera_360_velocity_msgs{"camera_360_velocity_topic"};
+
+    // ROS2 node and publishers for state publishing.
     std::shared_ptr<rclcpp::Node> ros_node_;
-    std::shared_ptr<rclcpp::Publisher<trajectory_msgs::msg::JointTrajectoryPoint>> trajectory_pub_;
+    std::shared_ptr<
+        rclcpp::Publisher<trajectory_msgs::msg::JointTrajectoryPoint>>
+        trajectory_prop_pub_;
+    std::shared_ptr<
+        rclcpp::Publisher<trajectory_msgs::msg::JointTrajectoryPoint>>
+        trajectory_cam_pub_;
+
+    // ROS2 subscriptions for receiving command messages.
+    rclcpp::Subscription<trajectory_msgs::msg::JointTrajectoryPoint>::SharedPtr
+        prop_command_sub_;
+    rclcpp::Subscription<trajectory_msgs::msg::JointTrajectoryPoint>::SharedPtr
+        camera_command_sub_;
+
+    // Store the latest received command messages.
+    std::optional<trajectory_msgs::msg::JointTrajectoryPoint>
+        commanded_propeller_;
+    std::optional<trajectory_msgs::msg::JointTrajectoryPoint> commanded_camera_;
+
+    // The model entity.
     gz::sim::Entity model_;
+
+    // The allowed joint names for each group (ordered as you want to map
+    // commands)
+    const std::vector<std::string> allowedPropellerJoints_ = {
+        "propeller_FL_link_joint", "propeller_FR_link_joint",
+        "propeller_BL_link_joint", "propeller_BR_link_joint"};
+
+    const std::vector<std::string> allowedCameraJoints_ = {
+        "camera_360_link_joint", "camera_joint_1_link_joint",
+        "camera_joint_2_link_joint"};
 };
--- drone_gazebo_plugins/src/channel_plugin.cc
+++ drone_gazebo_plugins/src/channel_plugin.cc
@@ -1,125 +1,279 @@
 #include "channel_plugin.hh"
 
-// This is required to register the plugin. Make sure the interfaces match what's in the header.
-GZ_ADD_PLUGIN(Channel,
-              gz::sim::System,
-              gz::sim::ISystemPostUpdate,
-              gz::sim::ISystemConfigure)
+// Register the plugin
+GZ_ADD_PLUGIN (Channel, gz::sim::System, gz::sim::ISystemPostUpdate,
+               gz::sim::ISystemConfigure)
 
-// This allows to use the plugin with the alias ChannelPlugin in the model file.
-GZ_ADD_PLUGIN_ALIAS(Channel, "gz::sim::system::ChannelPlugin")
+// Register the plugin alias
+GZ_ADD_PLUGIN_ALIAS (Channel, "gz::sim::system::ChannelPlugin")
 
-void Channel::Configure(const gz::sim::Entity &_entity,                  // _entity is the variable that contains the model information.
-                        const std::shared_ptr<const sdf::Element> &_sdf, // _sdf is the variable that contains the sdf information.
-                        gz::sim::EntityComponentManager &_ecm,           // _ecm is the variable that contains the entity component manager.
-                        gz::sim::EventManager & /*_eventMgr*/)
+//---------------------------------------------------------------------
+// Configure Method
+// Initializes the plugin by storing the model entity, printing the model name,
+// and creating two ROS2 publishers for the propellers and camera trajectories.
+//---------------------------------------------------------------------
+void Channel::Configure (const gz::sim::Entity &_entity,
+                         const std::shared_ptr<const sdf::Element> &_sdf,
+                         gz::sim::EntityComponentManager &_ecm,
+                         gz::sim::EventManager & /*_eventMgr*/)
 {
-  // Save the model in the model_ variable.
-  model_ = _entity;
 
-  // Initialize a variable with the model description.
-  const gz::sim::Model my_model = gz::sim::Model(this->model_);
-  ignmsg << "Model found: " << my_model.Name(_ecm) << std::endl;
+    // Save the model in the model_ variable.
+    model_ = _entity;
 
-  // Find the link with the name "base_link" in the model.
-  gz::sim::Entity link_ptr = my_model.LinkByName(_ecm, "base_link");
-  if (!link_ptr)
-  {
-    ignerr << "Link pointer not found" << std::endl;
-    return;
-  }
+    // Initialize a variable with the model description.
+    const gz::sim::Model my_model = gz::sim::Model (this->model_);
+    ignmsg << "Model found: " << my_model.Name (_ecm) << std::endl;
 
-  // Access the link with the name "base_link" in the model.
-  gz::sim::Link link_base = gz::sim::Link(link_ptr);
+    // Find the link with the name "base_link" in the model.
+    gz::sim::Entity link_ptr = my_model.LinkByName (_ecm, "base_link");
+    if (!link_ptr) {
+        ignerr << "Link pointer not found" << std::endl;
+        return;
+    }
 
-  // Get the joint with the name "propeller_FL_link_joint" in the model.
-  gz::sim::Entity propeller_FL_link_joint_ptr = my_model.JointByName(_ecm, "propeller_FL_link_joint");
-  if (!propeller_FL_link_joint_ptr)
-  {
-    ignerr << "Joint 'propeller_FL_link_joint' not found" << std::endl;
-    return;
-  }
+    // Access the link with the name "base_link" in the model.
+    gz::sim::Link link_base = gz::sim::Link (link_ptr);
 
-  // Get the joint with the name "propeller_FR_link_joint" in the model.
-  gz::sim::Entity propeller_FR_link_joint_ptr = my_model.JointByName(_ecm, "propeller_FR_link_joint");
-  if (!propeller_FR_link_joint_ptr)
-  {
-    ignerr << "Joint 'propeller_FR_link_joint' not found" << std::endl;
-    return;
-  }
+    // Get the joint with the name "propeller_FL_link_joint" in the model.
+    gz::sim::Entity propeller_FL_link_joint_ptr =
+        my_model.JointByName (_ecm, "propeller_FL_link_joint");
+    if (!propeller_FL_link_joint_ptr) {
+        ignerr << "Joint 'propeller_FL_link_joint' not found" << std::endl;
+        return;
+    }
 
-  // Get the joint with the name "propeller_BL_link_joint" in the model.
-  gz::sim::Entity propeller_BL_link_joint_ptr = my_model.JointByName(_ecm, "propeller_BL_link_joint");
-  if (!propeller_BL_link_joint_ptr)
-  {
-    ignerr << "Joint 'propeller_BL_link_joint' not found" << std::endl;
-    return;
-  }
+    // Get the joint with the name "propeller_FR_link_joint" in the model.
+    gz::sim::Entity propeller_FR_link_joint_ptr =
+        my_model.JointByName (_ecm, "propeller_FR_link_joint");
+    if (!propeller_FR_link_joint_ptr) {
+        ignerr << "Joint 'propeller_FR_link_joint' not found" << std::endl;
+        return;
+    }
 
-  // Get the joint with the name "propeller_BR_link_joint" in the model.
-  gz::sim::Entity propeller_BR_link_joint_ptr = my_model.JointByName(_ecm, "propeller_BR_link_joint");
-  if (!propeller_BR_link_joint_ptr)
-  {
-    ignerr << "Joint 'propeller_BR_link_joint' not found" << std::endl;
-    return;
-  }
+    // Get the joint with the name "propeller_BL_link_joint" in the model.
+    gz::sim::Entity propeller_BL_link_joint_ptr =
+        my_model.JointByName (_ecm, "propeller_BL_link_joint");
+    if (!propeller_BL_link_joint_ptr) {
+        ignerr << "Joint 'propeller_BL_link_joint' not found" << std::endl;
+        return;
+    }
 
-  // Now we have the link and the joints, we can get the pose of the link and the angular velocity of the joints.
-  if (!rclcpp::ok())
-  {
-    rclcpp::init(0, nullptr);
-  }
+    // Get the joint with the name "propeller_BR_link_joint" in the model.
+    gz::sim::Entity propeller_BR_link_joint_ptr =
+        my_model.JointByName (_ecm, "propeller_BR_link_joint");
+    if (!propeller_BR_link_joint_ptr) {
+        ignerr << "Joint 'propeller_BR_link_joint' not found" << std::endl;
+        return;
+    }
 
-  // Start the ROS2 node for the propeller trajectory.
-  this->ros_node_ = std::make_shared<rclcpp::Node>("channel_plugin_node");
-  this->trajectory_pub_ = ros_node_->create_publisher<trajectory_msgs::msg::JointTrajectoryPoint>(propeller_trajectory_node, 10);
+    // Get the joint with the name "camera_360_joint" in the model.
+    gz::sim::Entity camera_360_joint_ptr =
+        my_model.JointByName (_ecm, "camera_360_link_joint");
+    if (!camera_360_joint_ptr) {
+        ignerr << "Joint 'camera_360_joint' not found" << std::endl;
+        return;
+    }
 
+    // Get the joint with the name "camera_joint_1_link_joint" in the model.
+    gz::sim::Entity camera_joint_1_link_joint_ptr =
+        my_model.JointByName (_ecm, "camera_joint_1_link_joint");
+    if (!camera_joint_1_link_joint_ptr) {
+        ignerr << "Joint 'camera_joint_1_link_joint' not found" << std::endl;
+        return;
+    }
+
+    // Get the joint with the name "camera_joint_2_link_joint" in the model.
+    gz::sim::Entity camera_joint_2_link_joint_ptr =
+        my_model.JointByName (_ecm, "camera_joint_2_link_joint");
+    if (!camera_joint_2_link_joint_ptr) {
+        ignerr << "Joint 'camera_joint_2_link_joint' not found" << std::endl;
+        return;
+    }
+
+    // Get proppeller topic:
+    if (_sdf->HasElement ("propellers_velocity")) {
+        topic_propeller_velocity_msgs =
+            _sdf->Get<std::string> ("propellers_velocity");
+    }
+
+    // Get camera topic:
+    if (_sdf->HasElement ("camera_360_velocity")) {
+        topic_camera_360_velocity_msgs =
+            _sdf->Get<std::string> ("camera_360_velocity");
+    }
+
+    // Now we have the link and the joints, we can get the pose of the link and
+    // the angular velocity of the joints.
+    if (!rclcpp::ok ()) {
+        rclcpp::init (0, nullptr);
+    }
+
+    // Start the ROS2 node for the plugin.
+    this->ros_node_ = std::make_shared<rclcpp::Node> ("channel_plugin_node");
+
+    // Create the state publishers.
+    this->trajectory_prop_pub_ =
+        ros_node_
+            ->create_publisher<trajectory_msgs::msg::JointTrajectoryPoint> (
+                propeller_trajectory_node, 10);
+    this->trajectory_cam_pub_ =
+        ros_node_
+            ->create_publisher<trajectory_msgs::msg::JointTrajectoryPoint> (
+                camera_360_trajectory_node, 10);
+
+    // Create advertisement options for the propeller and camera topics.
+    this->pub_prop_ = node_.Advertise<gz::msgs::JointTrajectory> (
+        "/propellers_velocity_topic");
+    this->pub_cam_ = node_.Advertise<gz::msgs::JointTrajectory> (
+        "/camera_360_velocity_topic");
+
+    ignmsg << "Advertising Gazebo topics: /propellers_velocity_topic and "
+              "/camera_360_velocity_topic"
+           << std::endl;
 }
 
-// Here we implement the PostUpdate function, which is called at every iteration.
-void Channel::PostUpdate(const gz::sim::UpdateInfo& _info,const gz::sim::EntityComponentManager& _ecm)
+//---------------------------------------------------------------------
+// PostUpdate Method
+// 1) Publishes joint state to ROS 2 (existing logic).
+// 2) Applies commands if any (existing logic).
+// 3) Publishes minimal gz::msgs::JointTrajectory on two separate Gazebo topics.
+//---------------------------------------------------------------------
+void Channel::PostUpdate (const gz::sim::UpdateInfo &_info,
+                          const gz::sim::EntityComponentManager &_ecm)
 {
-  {
     if (_info.paused)
-      return;
+        return;
 
+    // 1) Build trajectory messages for ROS 2
+    trajectory_msgs::msg::JointTrajectoryPoint traj_prop_msg;
+    trajectory_msgs::msg::JointTrajectoryPoint traj_cam_msg;
 
-    std::unordered_set<std::string> allowedJoints = {
-      "propeller_FL_link_joint",
-      "propeller_FR_link_joint",
-      "propeller_BL_link_joint",
-      "propeller_BR_link_joint",
-      "camera_360_link_joint",
-      "camera_joint_1_link_joint",
-      "camera_joint_2_link_joint"
-    };
+    gz::sim::Model model (model_);
+    std::vector<gz::sim::Entity> joints = model.Joints (_ecm);
 
+    for (auto &jointEntity : joints) {
+        gz::sim::Joint joint (jointEntity);
+        auto maybeName = joint.Name (_ecm);
+        if (!maybeName.has_value ())
+            continue;
+        std::string jointName = maybeName.value ();
 
-    trajectory_msgs::msg::JointTrajectoryPoint traj_msg;
+        double position = 0.0, velocity = 0.0;
+        auto maybePosition = joint.Position (_ecm);
+        if (maybePosition.has_value () && !maybePosition.value ().empty ())
+            position = maybePosition.value ()[0];
+        auto maybeVelocity = joint.Velocity (_ecm);
+        if (maybeVelocity.has_value () && !maybeVelocity.value ().empty ())
+            velocity = maybeVelocity.value ()[0];
+        double acceleration = 0.0; // default
 
-    gz::sim::Model model(this->model_);
-
-    std::vector<gz::sim::Entity> joints = model.Joints(_ecm);
-
-    for (auto &jointEntity : joints)
-    {
-      gz::sim::Joint joint(jointEntity);
-      auto maybeName = joint.Name(_ecm);
-      if (!maybeName.has_value())
-        continue;
-      std::string jointName = maybeName.value();
-
-      if (allowedJoints.find(jointName) == allowedJoints.end())
-        continue;
-      auto maybeVelocity = joint.Velocity(_ecm);
-      double velocity = maybeVelocity.has_value() ? maybeVelocity.value()[0] : 0.0;
-      auto maybePosition = joint.Position(_ecm);
-      double position = maybePosition.has_value() ? maybePosition.value()[0] : 0.0;
-      double acceleration = 0.0;
-      traj_msg.positions.push_back(position);
-      traj_msg.velocities.push_back(velocity);
-      traj_msg.accelerations.push_back(acceleration);
+        // If joint belongs to propeller group
+        if (std::find (allowedPropellerJoints_.begin (),
+                       allowedPropellerJoints_.end (),
+                       jointName) != allowedPropellerJoints_.end ()) {
+            traj_prop_msg.positions.push_back (position);
+            traj_prop_msg.velocities.push_back (velocity);
+            traj_prop_msg.accelerations.push_back (acceleration);
+        }
+        // If joint belongs to camera group
+        else if (std::find (allowedCameraJoints_.begin (),
+                            allowedCameraJoints_.end (),
+                            jointName) != allowedCameraJoints_.end ()) {
+            traj_cam_msg.positions.push_back (position);
+            traj_cam_msg.velocities.push_back (velocity);
+            traj_cam_msg.accelerations.push_back (acceleration);
+        }
     }
-    trajectory_pub_->publish(traj_msg);
-  }
+
+    // Publish to ROS 2 if data is available
+    if (!traj_prop_msg.positions.empty ())
+        trajectory_prop_pub_->publish (traj_prop_msg);
+    if (!traj_cam_msg.positions.empty ())
+        trajectory_cam_pub_->publish (traj_cam_msg);
+
+    // 2) Command handling
+    if (commanded_propeller_.has_value ()) {
+        const auto &cmd = commanded_propeller_.value ();
+        if (cmd.velocities.size () >= allowedPropellerJoints_.size ()) {
+            for (size_t i = 0; i < allowedPropellerJoints_.size (); ++i) {
+                std::string jointName = allowedPropellerJoints_[i];
+                gz::sim::Entity jointEntity =
+                    model.JointByName (_ecm, jointName);
+                if (jointEntity == gz::sim::kNullEntity) {
+                    ignerr << "Propeller joint " << jointName
+                           << " not found for command update." << std::endl;
+                    continue;
+                }
+                gz::sim::Joint joint (jointEntity);
+                std::vector<double> commandVelocity = {cmd.velocities[i]};
+                // Update joint velocity
+                joint.SetVelocity (
+                    const_cast<gz::sim::EntityComponentManager &> (_ecm),
+                    commandVelocity);
+            }
+        }
+    }
+
+    if (commanded_camera_.has_value ()) {
+        const auto &cmd = commanded_camera_.value ();
+        if (cmd.positions.size () >= allowedCameraJoints_.size ()) {
+            for (size_t i = 0; i < allowedCameraJoints_.size (); ++i) {
+                std::string jointName = allowedCameraJoints_[i];
+                gz::sim::Entity jointEntity =
+                    model.JointByName (_ecm, jointName);
+                if (jointEntity == gz::sim::kNullEntity) {
+                    ignerr << "Camera joint " << jointName
+                           << " not found for command update." << std::endl;
+                    continue;
+                }
+                gz::sim::Joint joint (jointEntity);
+                std::vector<double> commandPosition = {cmd.positions[i]};
+                // Update joint position
+                joint.ResetPosition (
+                    const_cast<gz::sim::EntityComponentManager &> (_ecm),
+                    commandPosition);
+            }
+        }
+    }
+
+    // 3) Publish minimal JointTrajectory to each Gazebo topic
+    {
+        // For propellers
+        gz::msgs::JointTrajectory gzPropTraj;
+        auto *point = gzPropTraj.add_points ();
+        point->add_positions (0.1);  // example
+        point->add_velocities (0.2); // example
+        this->pub_prop_.Publish (gzPropTraj);
+    }
+    {
+        // For camera
+        gz::msgs::JointTrajectory gzCamTraj;
+        auto *point = gzCamTraj.add_points ();
+        point->add_positions (1.0);  // example
+        point->add_velocities (0.0); // example
+        this->pub_cam_.Publish (gzCamTraj);
+    }
+}
+
+//---------------------------------------------------------------------
+// Callback for propeller command subscription.
+// Stores the received propeller command message.
+//---------------------------------------------------------------------
+void Channel::PropellerCommandCallback (
+    const trajectory_msgs::msg::JointTrajectoryPoint::SharedPtr msg)
+{
+    commanded_propeller_ = *msg;
+    ignmsg << "Received propeller command" << std::endl;
+}
+
+//---------------------------------------------------------------------
+// Callback for camera command subscription.
+// Stores the received camera command message.
+//---------------------------------------------------------------------
+void Channel::CameraCommandCallback (
+    const trajectory_msgs::msg::JointTrajectoryPoint::SharedPtr msg)
+{
+    commanded_camera_ = *msg;
+    ignmsg << "Received camera command" << std::endl;
 }
